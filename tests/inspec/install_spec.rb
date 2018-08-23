b1abe2a016cb31da9a54cf2f11848956c30b0dc5
diodonfrost <antoine.mace@protonmail.com>
2018-08-23 19:40:02 +0200

    basic install of fail2ban on RedHat system

---
+++ tests/inspec/install_spec.rb
@@ -0,0 +1,20 @@
+# Fail2ban  check
+
+control 'install-01' do
+  impact 1.0
+  title 'Fail2ban install'
+  desc 'Fail2ban should be installed'
+  describe package('fail2ban') do
+    it { should be_installed }
+  end
+end
+
+control 'install-02' do
+  impact 1.0
+  title 'Fail2ban service'
+  desc 'Fail2ban should be enabled and started'
+  describe service('fail2ban') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
